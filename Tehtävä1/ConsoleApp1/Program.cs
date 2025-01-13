caac0d8e244938fd7287ceaff4d86f7f237f2c80
AVVR1 <nopolanopo@gmail.com>
2025-01-13 12:24:48 +0200

    ovi

--- Tehtävä1/ConsoleApp1/Program.cs
+++ Tehtävä1/ConsoleApp1/Program.cs
@@ -1,10 +1,29 @@
 ﻿namespace ConsoleApp1
 {
+    public enum Ovi { Auki, Kiinni, Lukossa };
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Ovi ovi = Ovi.Kiinni;
+            while (true)
+            {
+                Console.Write($"Ovi on {ovi}. Mitä haluat tehdä? ");
+                string action = Console.ReadLine();
+                if (action == "avaa" && ovi == Ovi.Kiinni)
+                {
+                    ovi = Ovi.Auki;
+                } else if (action == "lukitse" && ovi == Ovi.Kiinni)
+                {
+                    ovi = Ovi.Lukossa;
+                } else if (action == "sulje" && ovi == Ovi.Auki)
+                {
+                    ovi = Ovi.Kiinni;
+                } else if (action == "poista lukitus" && ovi == Ovi.Lukossa)
+                {
+                    ovi = Ovi.Kiinni;
+                }
+            }
         }
     }
 }
