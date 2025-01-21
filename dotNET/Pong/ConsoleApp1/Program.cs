fccefd7dda4ff4f28953b0c0d5333a19f677c98a
AVVR1 <nopolanopo@gmail.com>
2025-01-21 10:08:25 +0200

    Update Program.cs

--- dotNET/Pong/ConsoleApp1/Program.cs
+++ dotNET/Pong/ConsoleApp1/Program.cs
@@ -14,22 +14,23 @@ namespace ConsoleApp1
             #region muuttujat
             int windowWidth = Raylib.GetScreenWidth();
             int windowHeight = Raylib.GetScreenHeight();
+            Vector2 center = new Vector2(windowWidth/2,windowHeight/2);
 
-            float moveSpeed = 10;
+            float wallSpeed = 250;
             Vector2 wallSize = new Vector2(20, 100);
 
-            Vector2 p1Pos = new Vector2(10, windowHeight / 2 - wallSize.Y / 2);
+            Vector2 p1Pos = new Vector2(10, center.Y - wallSize.Y / 2);
             int p1Points = 0;
             Vector2 p1PointPos = new Vector2(windowWidth / 4, 0);
 
-            Vector2 p2Pos = new Vector2(windowWidth - wallSize.X - 10, windowHeight / 2 - wallSize.Y / 2);
+            Vector2 p2Pos = new Vector2(windowWidth - wallSize.X - 10, center.Y - wallSize.Y / 2);
             int p2Points = 0;
             Vector2 p2PointPos = new Vector2(windowWidth * 3/4, 0);
 
 
 
-            Vector2 ballPos = new Vector2(windowWidth / 2, windowHeight / 2);
-            Vector2 ballDir = new Vector2(1, 1);
+            Vector2 ballPos = center;
+            Vector2 ballDir = new Vector2(1, -1);
             float ballSpeed = 500;
 
             #endregion
@@ -42,8 +43,8 @@ namespace ConsoleApp1
                 Raylib.DrawRectangleV(p1Pos, wallSize, Color.White);
                 Raylib.DrawRectangleV(p2Pos, wallSize, Color.White);
 
-                Raylib.DrawTextEx(Raylib.GetFontDefault(), p1Points.ToString(), p1PointPos, 48, 0, Color.White);
-                Raylib.DrawTextEx(Raylib.GetFontDefault(), p2Points.ToString(), p2PointPos, 48, 0, Color.White);
+                Raylib.DrawTextEx(Raylib.GetFontDefault(), p1Points.ToString(), p1PointPos, 48, 5, Color.White);
+                Raylib.DrawTextEx(Raylib.GetFontDefault(), p2Points.ToString(), p2PointPos, 48, 5, Color.White);
 
                 Raylib.DrawCircleV(ballPos, 10, Color.White);
 
@@ -73,7 +74,34 @@ namespace ConsoleApp1
                     ballDir.Y = -ballDir.Y;
                 }
 
+                if (ballPos.X < 0)
+                {
+                    p2Points += 1;
+                    ballPos = center;
+                }
 
+                if (ballPos.X > windowWidth)
+                {
+                    p1Points += 1;
+                    ballPos = center;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.W))
+                {
+                    p1Pos.Y -= 1 * wallSpeed * Raylib.GetFrameTime();
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.S))
+                {
+                    p1Pos.Y += 1 * wallSpeed * Raylib.GetFrameTime();
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.Up))
+                {
+                    p2Pos.Y -= 1 * wallSpeed * Raylib.GetFrameTime();
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.Down))
+                {
+                    p2Pos.Y += 1 * wallSpeed * Raylib.GetFrameTime();
+                }
 
                 Raylib.EndDrawing();
 
