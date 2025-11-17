24fc34b3fe568b8a17fa732d894f79328d4ba5f7
AVVR1 <nopolanopo@gmail.com>
2025-11-17 14:08:44 +0200

    map image

--- dotNET/Cave_Shooter/Map.cs
+++ dotNET/Cave_Shooter/Map.cs
@@ -11,12 +11,12 @@ namespace Cave_Shooter
 	unsafe internal class Map
 	{
 		private Image image;
-		private Texture2D texture;
+		public Texture2D texture;
 		private Color* pixelDataPointer;
 
-		public Map(Image _image)
+		public Map(string filename)
 		{
-			image = _image;
+			image = Raylib.LoadImage(filename);
 			Raylib.ImageFormat(ref image, PixelFormat.UncompressedR8G8B8A8);
 			texture = Raylib.LoadTextureFromImage(image);
 		}
@@ -33,6 +33,11 @@ namespace Cave_Shooter
 			Raylib.ImageDrawCircleV(ref image, position, radius, color);
 		}
 
+		unsafe public void MapDrawRectangle(Rectangle rectangle, Color color)
+		{
+			Raylib.ImageDrawRectangleRec(ref image, rectangle, color);
+		}
+
 		unsafe public Color GetImageColor(Vector2 position)
 		{
 			if (position.X < 0 || position.X > image.Width || position.Y < 0 || position.Y > image.Height)
@@ -40,6 +45,11 @@ namespace Cave_Shooter
 				return Color.White;
 			}
 			return Raylib.GetImageColor(image, (int)position.X, (int)position.Y);
+		}
+
+		public void Draw()
+		{
+			Raylib.DrawTexture(texture, 0, 0, Color.White);
 		}
 	}
 }
